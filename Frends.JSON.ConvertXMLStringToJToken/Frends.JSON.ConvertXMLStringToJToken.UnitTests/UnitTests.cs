45af52a02a8f5ba404cc60d77f92b719658d6b7d
ttossavainen <teemu.tossavainen@hiq.fi>
2023-02-13 09:22:00 +0200

    init

---
+++ Frends.JSON.ConvertXMLStringToJToken/Frends.JSON.ConvertXMLStringToJToken.UnitTests/UnitTests.cs
@@ -0,0 +1,32 @@
+using Frends.JSON.ConvertXMLStringToJToken.Definitions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace Frends.JSON.ConvertXMLStringToJToken.UnitTests;
+
+[TestClass]
+public class UnitTests
+{
+    [TestMethod]
+    public void ShouldConvertXmlStringToJToken()
+    {
+        var input = new Input()
+        {
+            XML = @"<?xml version='1.0' standalone='no'?>
+             <root>
+               <person id='1'>
+                 <name>Alan</name>
+                 <url>http://www.google.com</url>
+               </person>
+               <person id='2'>
+                <name>Louis</name>
+                 <url>http://www.yahoo.com</url>
+              </person>
+            </root>"
+        };
+
+        var result = JSON.ConvertXMLStringToJToken(input);
+        Assert.IsTrue(result.Success);
+        Assert.IsInstanceOfType(result.Jtoken, typeof(JObject));
+    }
+}
---
+++ Frends.JSON.ConvertXMLStringToJToken/README.md
@@ -0,0 +1,26 @@
+# Frends.JSON.ConvertXMLStringToJToken
+Frends Task to ConvertXMLStringToJToken a JSON string from object or a JSON string.
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT) 
+[![Build](https://github.com/FrendsPlatform/Frends.JSON/actions/workflows/ConvertXMLStringToJToken_build_and_test_on_main.yml/badge.svg)](https://github.com/FrendsPlatform/Frends.JSON/actions)
+![Coverage](https://app-github-custom-badges.azurewebsites.net/Badge?key=FrendsPlatform/Frends.JSON/Frends.JSON.ConvertXMLStringToJToken|main)
+
+# Installing
+
+You can install the Task via Frends UI Task View.
+
+## Building
+
+
+Rebuild the project
+
+`dotnet build`
+
+Run tests
+ 
+`dotnet test`
+
+
+ConvertXMLStringToJToken a NuGet package
+
+`dotnet pack --configuration Release`
