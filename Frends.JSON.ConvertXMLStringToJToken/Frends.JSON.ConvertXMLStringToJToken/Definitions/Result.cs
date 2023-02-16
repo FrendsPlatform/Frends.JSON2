a35bd6f5cd41890611f80affb00e6404aed53991
ttossavainen <teemu.tossavainen@hiq.fi>
2023-02-16 15:16:47 +0200

    init

---
+++ Frends.JSON.ConvertXMLStringToJToken/Frends.JSON.ConvertXMLStringToJToken/Definitions/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace Frends.JSON.ConvertXMLStringToJToken.Definitions;
+
+/// <summary>
+/// Task's result.
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// Operation complete without errors.
+    /// </summary>
+    /// <example>true</example>
+    public bool Success { get; private set; }
+
+    /// <summary>
+    /// JToken.
+    /// </summary>
+    /// <example>{{ "?xml": { "@version": "1.0", "@standalone": "no" }, "root": { "foos": { "@id": "1", "foo": "bar" } }}}</example>
+    public object Jtoken { get; private set; }
+
+    internal Result(bool success, object jtoken)
+    {
+        Success = success;
+        Jtoken = jtoken;
+    }
+}
